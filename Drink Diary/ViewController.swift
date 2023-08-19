844a609545b3addb3ea979e3b0d7cb772ca15804
Satyam Sovan Mishra <msatyamsovan@gmail.com>
2023-08-19 19:14:35 +0530

    Initial Commit

---
+++ Drink Diary/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Drink Diary
+//
+//  Created by Satyam Sovan Mishra on 19/08/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
